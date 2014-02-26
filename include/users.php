5a00076547f6b19eac8df17ef82bc355340da865
Alexander <urichalex@mail.ru>
2014-02-26 04:30:42 -0800

    Палево =)

--- include/users.php
+++ include/users.php
@@ -2,12 +2,12 @@
 
 return array(
 	'admin' => array(
-		'email' => 'urichalex@mail.ru',
+		'email' => 'user@example.com',
 		'password' => 'e10adc3949ba59abbe56e057f20f883e',
 		'admin' => TRUE
 	),
 	'user' => array(
-		'email' => 'balamut81@mail.ru',
+		'email' => 'user2@example.com',
 		'password' => 'a01610228fe998f515a72dd730294d87',
 		'admin' => TRUE
 	),
